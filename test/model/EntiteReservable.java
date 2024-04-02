0b2a455ee173b4602aa6a66551b1171cf0b69fa2
Ulyashikk <ulianachernysheva1@gmail.com>
2024-04-02 15:26:09 +0200

    Entite reservable

---
+++ test/model/EntiteReservable.java
@@ -0,0 +1,28 @@
+package model;
+
+public abstract class EntiteReservable <T extends Formulaire>{
+	
+	private int numero;
+	private CalendrierAnnuel calendrier;
+
+	public EntiteReservable(int numero, CalendrierAnnuel calendrier) {
+		this.numero = numero;
+		this.calendrier = calendrier;
+	}
+	
+	public int getNumero() {
+		return numero;
+	}
+	
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	
+	public boolean estLibre(T formulaire) {
+		return calendrier.estLibre(formulaire.getJour(), formulaire.getMois());
+	}
+	
+	public abstract boolean compatible(T formulaire);
+	public abstract void reserver(T formulaire);
+
+}
